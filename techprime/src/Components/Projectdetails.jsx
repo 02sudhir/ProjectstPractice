095d2fbd0f70fc759e3c61973eecfbd9d48b0e8f
sudhir <sudhira@gmail.com>
2024-05-13 20:49:27 +0530

    first commit

---
+++ techprime/src/Components/Projectdetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+function Projectdetails() {
+  return (
+    <>
+        <Navbar />
+        <h1>dsasd</h1>
+    </>
+  )
+}
+
+export default Projectdetails
